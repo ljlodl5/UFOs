bb264bb56eb0a2c15b51917b25df34179cb5a6f1
LJ <ljlodl5@att.net>
2021-11-18 04:16:29 -0600

    ufo

--- static/js/app.js
+++ static/js/app.js
@@ -7,4 +7,33 @@ function printHello() {
     console.log("Hello there!");
   }
  
-  
+  function buildTable(data) {
+    tbody.html("");
+  }
+
+  data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
+
+  function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+
+// Build the table when the page loads
+buildTable(tableData);
