4ab5c56a2f73813c195788e34e0e95d292730746
LJ <ljlodl5@att.net>
2021-11-18 06:12:47 -0600

    read

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,4 @@
-// import the data from data.js
-const tableData = data;
-// Reference the HTML table using d3
-var tbody = d3.select("tbody");
-// Simple JavaScript console.log statement
-function printHello() {
-    console.log("Hello there!");
-  }
- 
+
   function buildTable(data) {
     tbody.html("");
   }
